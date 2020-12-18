a72432ff2826cd47e78356532189b91d8d5fa0b5
devkeita <keita.mywork@gmail.com>
2020-12-18 13:47:18 +0900

    add liff type and liff cdn

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,29 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import Head from "next/head";
 
 const Index = () => {
+    const [isInit, setInit] = useState(false);
+    const [isLogin, setLogin] = useState(false);
+
+    useEffect(() => {
+        if (isInit) return;
+        liff.init({ liffId: process.env.LIFF_ID })
+            .then(() => {
+                setInit(true);
+                setLogin(liff.isLoggedIn());
+            })
+        ;
+    });
+
     return (
-        <div>Hello, Nextjs!</div>
+        <React.Fragment>
+            <Head>
+                <script charSet="utf-8" src="https://static.line-scdn.net/liff/edge/2/sdk.js"></script>
+            </Head>
+            <div>Hello, Nextjs!</div>
+            {isLogin && <div>ログイン済み</div>}
+        </React.Fragment>
     );
-}
+};
 
 export default Index;
